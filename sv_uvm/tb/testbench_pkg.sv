f8286fb7ed8915e08723674b7519b6c17ea4b4c0
MihirDonga <117023016+MihirDonga@users.noreply.github.com>
2025-06-15 01:49:11 +0200

    addsdfcd

--- sv_uvm/tb/testbench_pkg.sv
+++ sv_uvm/tb/testbench_pkg.sv
@@ -5,41 +5,41 @@
 package testbench_pkg;
   import uvm_pkg::*;
   `include "uvm_macros.svh"
-  `include "apb_uart_tb.sv"
-  // // UART
-  // `include "uart_config.sv"
-  // `include "apb_config.sv"
-  // `include "uart_transaction.sv"
-  // `include "apb_transaction.sv"
-  // `include "uart_sequencer.sv"
-  // `include "apb_sequencer.sv"
-  // `include "uart_sequence.sv"
-  // `include "apb_sequence.sv"
-  // `include "uart_driver.sv"
-  // `include "apb_driver.sv"
-  // `include "uart_monitor.sv"
-  // `include "apb_monitor.sv"
-  // `include "uart_agent.sv"
-  // `include "apb_agent.sv"
+  // `include "apb_uart_tb.sv"
+  // UART
+  `include "uart_config.sv"
+  `include "apb_config.sv"
+  `include "uart_transaction.sv"
+  `include "apb_transaction.sv"
+  `include "uart_sequencer.sv"
+  `include "apb_sequencer.sv"
+  `include "uart_sequence.sv"
+  `include "apb_sequence.sv"
+  `include "uart_driver.sv"
+  `include "apb_driver.sv"
+  `include "uart_monitor.sv"
+  `include "apb_monitor.sv"
+  `include "uart_agent.sv"
+  `include "apb_agent.sv"
 
-  // // Virtual Sequencer & Sequences
-  // `include "apbuart_vsequencer.sv"
-  // `include "apbuart_vseq_base.sv"
+  // Virtual Sequencer & Sequences
+  `include "apbuart_vsequencer.sv"
+  `include "apbuart_vseq_base.sv"
 
 
-  // // Scoreboard
-  // `include "apbuart_scoreboard.sv"
+  // Scoreboard
+  `include "apbuart_scoreboard.sv"
 
-  // // Tests
-  // `include "apbuart_environment.sv"
-  // `include "apbuart_base_test.sv"
-  // `include "apbuart_config_test.sv"
-  // `include "apbuart_data_compare_test.sv"
-  // `include "apbuart_frame_error_test.sv"
-  // `include "apbuart_free_error_test.sv"
-  // `include "apbuart_parity_error_test.sv"
-  // `include "apbuart_rec_drv_test.sv"
-  // `include "apbuart_rec_readreg_test.sv"
+  // Tests
+  `include "apbuart_environment.sv"
+  `include "apbuart_base_test.sv"
+  `include "apbuart_config_test.sv"
+  `include "apbuart_data_compare_test.sv"
+  `include "apbuart_frame_error_test.sv"
+  `include "apbuart_free_error_test.sv"
+  `include "apbuart_parity_error_test.sv"
+  `include "apbuart_rec_drv_test.sv"
+  `include "apbuart_rec_readreg_test.sv"
 
 
 endpackage : testbench_pkg
