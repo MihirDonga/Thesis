e4722f6d869e6b3f9c1962e9966bb0e5d2b0a22a
MihirDonga <117023016+MihirDonga@users.noreply.github.com>
2025-06-16 11:52:53 +0200

    addsdfcd

--- sv_uvm/tb/uart_monitor.sv
+++ sv_uvm/tb/uart_monitor.sv
@@ -4,46 +4,46 @@ class uart_monitor extends uvm_monitor;
   
 	`uvm_component_utils(uart_monitor)
 
-// ✅ Coverage struct
-typedef struct {
-  logic [31:0] transmitter_reg;
-  logic        parity_en;
-  logic [6:0]  frame_len;
-  logic [3:0]  n_sb;
-} uart_cov_data_t;
+	// ✅ Coverage struct
+	typedef struct {
+	logic [31:0] transmitter_reg;
+	logic        parity_en;
+	logic [6:0]  frame_len;
+	logic [3:0]  n_sb;
+	} uart_cov_data_t;
 
-// ✅ Covergroup definition
-covergroup uart_cov_type_t with function sample(uart_cov_data_t data);
+	// ✅ Covergroup definition
+	covergroup uart_cov_type_t with function sample(uart_cov_data_t data);
 
-  option.per_instance = 1;
-  
-  cp_transmitter_reg : coverpoint data.transmitter_reg {
-    bins all_zero = {32'h0000_0000};
-    bins all_ones = {32'hFFFF_FFFF};
-    bins alternating = {32'hAAAAAAAA, 32'h55555555};
-    bins misc = default;
-  }
+	option.per_instance = 1;
+	
+	cp_transmitter_reg : coverpoint data.transmitter_reg {
+		bins all_zero = {32'h0000_0000};
+		bins all_ones = {32'hFFFF_FFFF};
+		bins alternating = {32'hAAAAAAAA, 32'h55555555};
+		bins misc = default;
+	}
 
-  cp_parity_en : coverpoint data.parity_en {
-    bins disabled = {0};
-    bins enabled = {1};
-  }
+	cp_parity_en : coverpoint data.parity_en {
+		bins disabled = {0};
+		bins enabled = {1};
+	}
 
-  cp_frame_len : coverpoint data.frame_len {
-    bins len5 = {5};
-    bins len6 = {6};
-    bins len7 = {7};
-    bins len8 = {8};
-    bins len9 = {9};
-  }
+	cp_frame_len : coverpoint data.frame_len {
+		bins len5 = {5};
+		bins len6 = {6};
+		bins len7 = {7};
+		bins len8 = {8};
+		bins len9 = {9};
+	}
 
-  cp_n_sb : coverpoint data.n_sb {
-    bins one_stop = {0};
-    bins two_stop = {1};
-  }
+	cp_n_sb : coverpoint data.n_sb {
+		bins one_stop = {0};
+		bins two_stop = {1};
+	}
 
 
-endgroup
+	endgroup
 
   	// ---------------------------------------
   	//  Virtual Interface
