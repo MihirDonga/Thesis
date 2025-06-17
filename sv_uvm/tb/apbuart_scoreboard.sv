ee83d3f4066f7e4d9f63e791058e0ba1d330e694
MihirDonga <117023016+MihirDonga@users.noreply.github.com>
2025-06-17 11:19:37 +0200

    addsdfcd

--- sv_uvm/tb/apbuart_scoreboard.sv
+++ sv_uvm/tb/apbuart_scoreboard.sv
@@ -359,7 +359,7 @@ class apbuart_scoreboard extends uvm_scoreboard;
 
 		// Cross config coverage
 		cfg_cross: cross baud_cp, frame_cp, parity_cp, stopbit_cp{ignore_bins illegal_combos = binsof(baud_cp) intersect {0, 63} && binsof(stopbit_cp) intersect {2};}
-		baud_parity_cross: cross baud_cp, parity_cp;
+		// baud_parity_cross: cross baud_cp, parity_cp;
 	endgroup
 
 	// covergroup tx_cg;
