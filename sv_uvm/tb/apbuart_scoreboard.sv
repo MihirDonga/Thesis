34b82721d0b6319712e38ab88c1884d70f0bbcf0
MihirDonga <117023016+MihirDonga@users.noreply.github.com>
2025-06-17 18:53:47 +0200

    gfsdf

--- sv_uvm/tb/apbuart_scoreboard.sv
+++ sv_uvm/tb/apbuart_scoreboard.sv
@@ -634,15 +634,6 @@ function void apbuart_scoreboard::compare_receive (apb_transaction apb_pkt , uar
 	`uvm_info(get_type_name(),$sformatf("Expected Error Value : %0h Actual Error Value: %0h",1'b1,apb_pkt.PSLVERR),UVM_LOW)
 	`uvm_info(get_type_name(),"------------------------------------\n",UVM_LOW)
 	end
-	else begin
-		if(err_actual == err_expected)
-			`uvm_info(get_type_name(),$sformatf("------ :: Error Match :: ------"),UVM_LOW)
-		else
-			`uvm_error(get_type_name(),$sformatf("------ :: Error MisMatch :: ------"))
-		`uvm_info(get_type_name(),$sformatf("Expected Error Value : %0h Actual Error Value: %0h",1'b0,apb_pkt.PSLVERR),UVM_LOW)
-		`uvm_info(get_type_name(),"------------------------------------\n",UVM_LOW)
-		
-	end
 	rx_cg.sample(apb_pkt.PRDATA, uart_pkt.payload, err_actual);
 	rx_sample_count++;
 endfunction
