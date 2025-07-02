cf38e93dd0317076643e8e7b8c590b693bc76e9d
MihirDonga <117023016+MihirDonga@users.noreply.github.com>
2025-07-02 10:58:35 +0200

    PYVCS CONSTRAINT AND COVERAGE AAND CG AND CP ADDED

--- cocotb_pyuvm/sim/apbuart_config_test.py
+++ cocotb_pyuvm/sim/apbuart_config_test.py
@@ -9,6 +9,7 @@ class apbuart_config_test(apbuart_base_test):
         self.apbuart_config_sq = None
 
     def build_phase(self, phase):
+        uvm_factory().set_inst_override_by_type(apbuart_base_test,apbuart_config_test)
         print(f"Entering build_phase for {self.get_name()}")
         super().build_phase(phase)
         self.apbuart_config_sq = apbuart_config_seq.type_id.create("apbuart_config_sq")
