58305006b74297ef3cab6de0766d67ee22b23130
MihirDonga <117023016+MihirDonga@users.noreply.github.com>
2025-07-02 16:51:14 +0200

    PYVCS CONSTRAINT AND COVERAGE AAND CG AND CP ADDED

--- cocotb_pyuvm/sim/testbench.py
+++ cocotb_pyuvm/sim/testbench.py
@@ -5,21 +5,26 @@ from pyuvm import *
 # from apbuart_config_test import apbuart_config_test  
 from apbuart_base_test import apbuart_config_test
 # Simple version without interface classes - direct signal access
+# @cocotb.test()
+# async def tbench_top(dut):
+    
+#     # Clock Generation: 50 MHz (20 ns period)
+#     cocotb.start_soon(Clock(dut.PCLK, 20, units="ns").start())
+
+#     # Reset Generation (Active Low)
+#     dut.PRESETn.value = 0
+#     await Timer(100, units="ns")  # Hold reset low for 100 ns
+#     dut.PRESETn.value = 1
+#     await RisingEdge(dut.PCLK)
+
+#     # Store signals in ConfigDB for access by UVM components
+#     ConfigDB().set(None, "*", "dut", dut)  # You can pass entire DUT for convenience
+
+#     # Start UVM test (equivalent to run_test())
+#     await uvm_root().run_test("apbuart_config_test")
 @cocotb.test()
 async def tbench_top(dut):
-    
-    # Clock Generation: 50 MHz (20 ns period)
-    cocotb.start_soon(Clock(dut.PCLK, 20, units="ns").start())
-
-    # Reset Generation (Active Low)
-    dut.PRESETn.value = 0
-    await Timer(100, units="ns")  # Hold reset low for 100 ns
-    dut.PRESETn.value = 1
-    await RisingEdge(dut.PCLK)
-
-    # Store signals in ConfigDB for access by UVM components
-    ConfigDB().set(None, "*", "dut", dut)  # You can pass entire DUT for convenience
-
-    # Start UVM test (equivalent to run_test())
+    from pyuvm import uvm_root
+    root = uvm_root()
+    print("Registered test classes:", root._uvm_factory._registry.keys())
     await uvm_root().run_test("apbuart_config_test")
- 
